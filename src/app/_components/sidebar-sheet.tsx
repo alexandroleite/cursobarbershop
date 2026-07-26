0ed10efd2ee9970363808b5e076e08ca5291a340
Alexqandro N Leite <alexandronardeli@hotmail.com>
2026-07-26 09:03:03 -0400

    chore: add next auth setup

--- src/app/_components/sidebar-sheet.tsx
+++ src/app/_components/sidebar-sheet.tsx
@@ -1,10 +1,17 @@
 import { Button } from "./ui/button"
-import { CalendarIcon, HomeIcon, LogOutIcon } from "lucide-react"
+import { CalendarIcon, HomeIcon, LogInIcon, LogOutIcon } from "lucide-react"
 import { SheetClose, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet"
 import { quickSearchOptions } from "../_constants/search"
-import { Avatar, AvatarImage } from "./ui/avatar"
 import Link from "next/link"
 import Image from "next/image"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog"
 
 const SidebarSheet = () => {
   return (
@@ -14,14 +21,44 @@ const SidebarSheet = () => {
       </SheetHeader>
 
       {/* container avatar */}
-      <div className="flex items-center gap-3 border-b border-solid px-2 pb-3">
-        <Avatar>
+      <div className="flex items-center justify-between gap-3 border-b border-solid px-2 pb-3">
+        <h2 className="font-bold">Olá faça seu login</h2>
+
+        <Dialog>
+          <DialogTrigger
+            render={
+              <Button size="icon">
+                <LogInIcon size={18} />
+              </Button>
+            }
+          />
+
+          <DialogContent className="w-[90%]">
+            <DialogHeader>
+              <DialogTitle>Faça login na plataforma</DialogTitle>
+              <DialogDescription>
+                Conecte-se usando sua conta do Google
+              </DialogDescription>
+              <Button variant="outline" className="gap-1 font-bold">
+                <Image
+                  src="/google.svg"
+                  height={18}
+                  width={18}
+                  alt="Fazer login com o google"
+                />
+                Google
+              </Button>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
+
+        {/* <Avatar>
           <AvatarImage src="/jin-woo.jpg" />
         </Avatar>
         <div>
           <p className="font-bold">Alexandro N Leite</p>
           <p className="text-xs">Alexandroleite@hotmail.com</p>
-        </div>
+        </div> */}
       </div>
 
       {/* renderiza botao inicio e agendamento */}
